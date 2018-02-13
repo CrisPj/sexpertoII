9d87c192b7b31cb9f5ed3ba72a5f863da8b8e1f3
Carmona <13030617@itcelaya.edu.mx>
2018-02-12 23:35:31 -0600

    Primera seccion de mejoras a la interfaz del Index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,82 @@
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">
-        sexperto2
-      </h1>
-      <h2 class="subtitle">
-        Sexperto II
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+<section class="container">
+  <div>
+    <h1 class="title">
+    Desarrollos Inteligentes.
+    </h1>
+    <h2 class="subtitle">
+    Instituto Tecnlógico de Celaya
+    </h2>
+    <p>
+      El presente sistema experto presenta una serie de mejoras, con base en las observaciones hechas a las areas de oportunidad
+    </p>
+    <br>
+    <div class="box">
+      <article class="media">
+        <div class="media-left">
+          
+        </div>
+        <div class="media-content">
+          <div class="content">
+            <p><strong><h3>Tabla de hechos</h3></strong></p>
+            <p><small>En la presente tabla se introducen los hechos a trabajar sobre 
+                      la base de conocimiento ya establecida.
+            </small></p>
+
+
+<table class="table">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Hecho</th>
+      <th>Seleccion</th>
+      <th>Borrar</th>
+    </tr>
+  </thead>
+  <tfoot>
+    <tr>
+      <th>ID</th>
+      <th>Hecho</th>
+      <th>Seleccion</th>
+      <th>Borrar</th>
+    </tr>
+  </tfoot>
+  <tbody>
+    <!---
+    <div v-for="regla in reglas">
+      <tr>
+      <th>{{regla.llave}}</th>
+      <th>{{regla.hecho}}</th>
+      <td>[]</td>
+      <td>X</td>
+    </tr>  
     </div>
-  </section>
+    -->
+  </tbody>
+</table>
+
+          </div>
+          <nav class="level is-mobile">
+            <div class="level-left">
+              <a class="level-item">
+                <span class="icon is-small"><i class="fas fa-reply"></i></span>
+              </a>
+              <a class="level-item">
+                <span class="icon is-small"><i class="fas fa-retweet"></i></span>
+              </a>
+              <a class="level-item">
+                <span class="icon is-small"><i class="fas fa-heart"></i></span>
+              </a>
+            </div>
+          </nav>
+        </div>
+      </article>
+    </div>
+
+
+  </div>
+</section>
 </template>
+<script>
+  
+</script>
