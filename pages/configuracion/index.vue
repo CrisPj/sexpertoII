9ecb745e3bcaa0a05567b4048257d2358e6786aa
Carmona <13030617@itcelaya.edu.mx>
2018-02-13 19:57:49 -0600

    Cambios a vista, se agregan modal y boton de reglas y cositas

--- pages/configuracion/index.vue
+++ pages/configuracion/index.vue
@@ -17,60 +17,56 @@
 						<p><strong><h3>Configuración</h3></strong></p>
 						<p>Esta sección esta dedicada a la configuración del sistema experto, aspectos relacionados con la <strong>Base de Conocimiento, Base de Hechos</strong>, ádemas de proporcionar una vista sobre el conococimiento con el que se trabaja.
 						</p>
+						<br>
 					</div>
-					<nav class="level is-mobile">
-						<div class="level-left">
-							<a class="level-item">
-								<span class="icon is-small"><i class="fas fa-reply"></i></span>
-							</a>
-							<a class="level-item">
-								<span class="icon is-small"><i class="fas fa-retweet"></i></span>
-							</a>
-							<a class="level-item">
-								<span class="icon is-small"><i class="fas fa-heart"></i></span>
-							</a>
-						</div>
-					</nav>
 				</div>
 			</article>
 		</div>
-		<div class="card">
-			<header class="card-header">
-				<p class="card-header-title">
-					Ver
-				</p>
+		
+		<div class="columns">
+			<div class="column">
 				
-			</header>
-			<div class="card-content">
-				<div class="content">
-						<a class="button is-info">Ver Reglas</a>
-						&nbsp; &nbsp; &nbsp; &nbsp;
-						<a class="button is-info">Ver Hechos (Antecedentes)</a>
+				<div class="card">
+					<header class="card-header">
+						<p class="card-header-title">
+							Ver
+						</p>
+						
+					</header>
+					<div class="card-content">
+						<div class="content">
+							<a class="button is-info">Ver Reglas</a>
+							&nbsp; &nbsp; &nbsp; &nbsp;
+							<a class="button is-info">Ver Hechos (Antecedentes)</a>
+						</div>
+					</div>
+					<footer class="card-footer">
+						
+					</footer>
 				</div>
 			</div>
-			<footer class="card-footer">
-				
-			</footer>
-		</div>
-<br>
-		<div class="card">
-			<header class="card-header">
-				<p class="card-header-title">
-					Agregar
-				</p>
-				
-			</header>
-			<div class="card-content">
-				<div class="content">
-						<a class="button is-success">Añadir más reglas a la base de hechos</a>
-						&nbsp; &nbsp; &nbsp; &nbsp;
+			<br>
+			<div class="column">
+				<div class="card">
+					<header class="card-header">
+						<p class="card-header-title">
+							Agregar
+						</p>
+						
+					</header>
+					<div class="card-content">
+						<div class="content">
+							<a class="button is-success">Añadir más reglas a la base de hechos</a>
+							&nbsp; &nbsp; &nbsp; &nbsp;
+						</div>
+					</div>
+					<footer class="card-footer">
+						
+					</footer>
 				</div>
+				<br>
 			</div>
-			<footer class="card-footer">
-				
-			</footer>
 		</div>
-		<br>
 		<div class="card">
 			<header class="card-header">
 				<p class="card-header-title">
@@ -80,15 +76,14 @@
 			</header>
 			<div class="card-content">
 				<div class="content">
-				<a class="button is-warning">Borrar Reglas una a la vez</a>
-						&nbsp; &nbsp; &nbsp; &nbsp;
-						<a class="button is-danger">Borrar todas las  Reglas</a>
-						&nbsp; &nbsp; &nbsp; &nbsp;
-						<a class="button is-danger">Borrar todos los  Hechos</a>
+					<a class="button is-warning">Borrar Reglas una a la vez</a>
+					&nbsp; &nbsp; &nbsp; &nbsp;
+					<a class="button is-danger">Borrar todas las  Reglas</a>
+					&nbsp; &nbsp; &nbsp; &nbsp;
+					<a class="button is-danger">Borrar todos los  Hechos</a>
 				</div>
 			</div>
 			<footer class="card-footer">
-
 			</footer>
 		</div>
 		<br>
--- pages/index.vue
+++ pages/index.vue
@@ -7,40 +7,87 @@
     <h2 class="subtitle">
     Instituto Tecnlógico de Celaya
     </h2>
-    <p>
-      El presente sistema experto presenta una serie de mejoras, con base en las observaciones hechas a las areas de oportunidad
-    </p>
-    <br>
-
-    <b-modal :active.sync="isModalActive">
-      <form action="">
-        <div class="modal-card" style="width: auto">
-          <header class="modal-card-head">
-            <p class="modal-card-title">Login</p>
-          </header>
-          <section class="modal-card-body">
-            <b-field label="Hecho">
-              <b-input
-                type="text"
-                :value="hecho"
-                placeholder="p"
-                required>
-              </b-input>
-            </b-field>
-
-          </section>
-          <footer class="modal-card-foot">
-            <button class="button is-primary">Submit</button>
-          </footer>
-        </div>
-      </form>
-    </b-modal>
-
-
+    
     <div class="box">
       <article class="media">
         <div class="media-left">
-
+        </div>
+        <div class="media-content">
+          <div class="content">
+            <p><strong><h3>Sistema Experto Determinista.</h3></strong></p>
+            <p>
+              El presente sistema experto presenta una serie de mejoras, con base en las observaciones hechas a las areas de oportunidad.
+            </p>
+          </div>
+          
+        </div>
+      </article>
+    </div>
+    <b-modal :active.sync="isModalActive">
+    <form action="">
+      <div class="modal-card" style="width: auto">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Agregar Hecho</p>
+        </header>
+        <section class="modal-card-body">
+          <b-field label="Hecho">
+          <b-input
+          type="text"
+          :value="hecho"
+          placeholder="p"
+          required>
+          </b-input>
+          </b-field>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-primary">Submit</button>
+        </footer>
+      </div>
+    </form>
+    </b-modal>
+    <b-modal :active.sync="isModalActive2">
+    <form action="">
+      <div class="modal-card" style="width: auto">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Ver Reglas</p>
+        </header>
+        <section class="modal-card-body">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Regla</th>
+              </tr>
+            </thead>
+            <tfoot>
+            <tr>
+              <th>ID</th>
+              <th>Regla</th>
+            </tr>
+            </tfoot>
+            <tbody>
+              <!---
+              <div v-for="regla in reglas">
+                <tr>
+                  <th>{{regla.llave}}</th>
+                  <th>{{regla.hecho}}</th>
+                  <td>[]</td>
+                  <td>X</td>
+                </tr>
+              </div>
+              -->
+            </tbody>
+          </table>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-primary">Submit</button>
+        </footer>
+      </div>
+    </form>
+    </b-modal>
+    <div class="box">
+      <article class="media">
+        <div class="media-left">
         </div>
         <div class="media-content">
           <div class="content">
@@ -48,7 +95,11 @@
             <p><small>En la presente tabla se introducen los hechos a trabajar sobre
               la base de conocimiento ya establecida.
             </small></p>
-            <a @click="isModalActive = true" class="button is-success is-pulled-right">Agregar hecho</a>
+            <div class="is-pulled-right">
+              <a @click="isModalActive = true" class="button is-success">Agregar hecho</a>
+              &nbsp; &nbsp;
+              <a @click="isModalActive2 = true" class="button is-link">Ver Reglas</a>
+            </div>
             <table class="table">
               <thead>
                 <tr>
@@ -80,19 +131,7 @@
               </tbody>
             </table>
           </div>
-          <nav class="level is-mobile">
-            <div class="level-left">
-              <a class="level-item">
-                <span class="icon is-small"><i class="fas fa-reply"></i></span>
-              </a>
-              <a class="level-item">
-                <span class="icon is-small"><i class="fas fa-retweet"></i></span>
-              </a>
-              <a class="level-item">
-                <span class="icon is-small"><i class="fas fa-heart"></i></span>
-              </a>
-            </div>
-          </nav>
+          
         </div>
       </article>
     </div>
@@ -113,21 +152,8 @@
               &nbsp; &nbsp; &nbsp; &nbsp;
               <a class="button is-info is-link">Encadenamiento hacia atras</a>
             </div>
-
           </div>
-          <nav class="level is-mobile">
-            <div class="level-left">
-              <a class="level-item">
-                <span class="icon is-small"><i class="fas fa-reply"></i></span>
-              </a>
-              <a class="level-item">
-                <span class="icon is-small"><i class="fas fa-retweet"></i></span>
-              </a>
-              <a class="level-item">
-                <span class="icon is-small"><i class="fas fa-heart"></i></span>
-              </a>
-            </div>
-          </nav>
+          
         </div>
       </article>
     </div>
@@ -156,19 +182,7 @@
               </tbody>
             </table>
           </div>
-          <nav class="level is-mobile">
-            <div class="level-left">
-              <a class="level-item">
-                <span class="icon is-small"><i class="fas fa-reply"></i></span>
-              </a>
-              <a class="level-item">
-                <span class="icon is-small"><i class="fas fa-retweet"></i></span>
-              </a>
-              <a class="level-item">
-                <span class="icon is-small"><i class="fas fa-heart"></i></span>
-              </a>
-            </div>
-          </nav>
+          
         </div>
       </article>
     </div>
@@ -177,12 +191,13 @@
 </section>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        isModalActive: false,
-        hecho: null
-      }
-    }
-  }
+export default {
+data() {
+return {
+isModalActive: false,
+isModalActive2: false,
+hecho: null
+}
+}
+}
 </script>
